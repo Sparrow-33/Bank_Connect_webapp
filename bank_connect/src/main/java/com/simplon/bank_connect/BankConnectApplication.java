4873ffaaa692bf0d3497465e178e805f638b60f0
Oussama95el <oussamaelbechari@gmail.com>
2022-12-16 09:25:48 +0100

    creating spring boot project with the recommended dependecies

---
+++ bank_connect/src/main/java/com/simplon/bank_connect/BankConnectApplication.java
@@ -0,0 +1,13 @@
+package com.simplon.bank_connect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankConnectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankConnectApplication.class, args);
+	}
+
+}
