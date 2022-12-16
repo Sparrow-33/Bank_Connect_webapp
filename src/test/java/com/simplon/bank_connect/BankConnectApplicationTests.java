2d3f406a2fd7215852123f1bd73fc6a0a34b9c3e
Oussama95el <oussamaelbechari@gmail.com>
2022-12-16 09:25:48 +0100

    creating spring boot project with the recommended dependecies

---
+++ src/test/java/com/simplon/bank_connect/BankConnectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simplon.bank_connect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankConnectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
