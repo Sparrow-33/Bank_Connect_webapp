45af68839e0fea92e80f50cd9fb66db31b02e93c
Oussama95el <oussamaelbechari@gmail.com>
2022-12-20 20:08:31 +0100

    updating entities and test insert and get data using jpa repository

--- src/main/java/com/simplon/bank_connect/client/ClientController.java
+++ src/main/java/com/simplon/bank_connect/client/ClientController.java
@@ -1,17 +1,28 @@
 package com.simplon.bank_connect.client;
 
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/client")
 public class ClientController {
 
+    private final ClientService clientService;
+
+    public ClientController(ClientService clientService) {
+        this.clientService = clientService;
+    }
 
     @GetMapping
     public String hello() {
         return "Hello Oussama";
+    }
+
+
+    @PostMapping("/add")
+    public Client saveClient(@RequestBody Client client) {
+        System.out.println(client);
+        return clientService.saveClient(client);
     }
 }
