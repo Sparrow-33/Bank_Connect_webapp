99800891dbbc14b7fc731aab9037a1910ad4e961
Oussama95el <oussamaelbechari@gmail.com>
2022-12-21 09:56:59 +0100

    adding methods to api controllers

--- src/main/java/com/simplon/bank_connect/client/ClientController.java
+++ src/main/java/com/simplon/bank_connect/client/ClientController.java
@@ -4,6 +4,8 @@ package com.simplon.bank_connect.client;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/client")
 public class ClientController {
@@ -14,11 +16,22 @@ public class ClientController {
         this.clientService = clientService;
     }
 
-    @GetMapping
-    public String hello() {
-        return "Hello Oussama";
+    @GetMapping("/all")
+    public List<Client> getAllClients() {
+        return clientService.getAllClients();
     }
-
+    //get client by id
+    @GetMapping("/find/{id}")
+    public Client getClientById(@PathVariable("id") Long id) {
+        System.out.println("id = " + id);
+        return clientService.getClientById(id);
+    }
+//get client by cin
+    @GetMapping("/find")
+    public Client getClientByCin(@RequestParam("cin") String cin) {
+        System.out.println("cin = " + cin);
+        return clientService.getClientByCin(cin);
+    }
 
     @PostMapping("/add")
     public Client saveClient(@RequestBody Client client) {
